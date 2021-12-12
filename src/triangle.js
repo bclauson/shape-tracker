33135eb6997d41101d0c8a4ba28c7382704c50ba
bclauson <clauson.branden@gmail.com>
2021-12-12 15:30:06 -0800

    full testing of Triangle with Jest

--- src/triangle.js
+++ src/triangle.js
@@ -1,8 +1,7 @@
-export default function Triangle(side1, side2, side3, height) {
+export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
-  this.isTriangle = ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) ? "not a triangle" : "this is a triangle"; 
 }
 
 Triangle.prototype.checkType = function() {
@@ -10,8 +9,8 @@ Triangle.prototype.checkType = function() {
     return "not a triangle";
   } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
     return "scalene triangle";
-  } else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
-      return "equilateral triangle";
+  }  else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
+    return "equilateral triangle";
   } else {
     return "isosceles triangle";
   }
